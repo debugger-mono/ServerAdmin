ebf88145550e7efc6b7c750ba6067c823233349a
Nitin Mittal <nitin-mittal@hotmail.com>
2015-07-01 22:02:41 +0100

    Adding missing files

---
+++ ServerAdmin/Controllers/TemplatesController.cs
@@ -0,0 +1,62 @@
+﻿using System.Web.Mvc;
+
+namespace ServerAdmin.Controllers
+{
+    public class TemplatesController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Main()
+        {
+            return View();
+        }
+
+        public ActionResult Header()
+        {
+            return View();
+        }
+
+        public ActionResult HeaderNotification()
+        {
+            return View();
+        }
+
+        public ActionResult Sidebar()
+        {
+            return View();
+        }
+
+        public ActionResult SidebarSearch()
+        {
+            return View();
+        }
+
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public ActionResult Stat()
+        {
+            return View();
+        }
+
+        public ActionResult Timeline()
+        {
+            return View();
+        }
+
+        public ActionResult Notification()
+        {
+            return View();
+        }
+
+        public ActionResult Chat()
+        {
+            return View();
+        }
+    }
+}
